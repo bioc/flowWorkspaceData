e55239b4f08d6af17784b4c88dd49bf185b9dea2
Chao-Jen Wong <cwon2@fhcrc.org>
2011-05-05 17:36:46 +0000

    Added the flowWorkspaceData package to the repository.
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/flowWorkspaceData@1564 db2202da-8704-0410-b924-b125b3cd84ef

---
+++ R/flowWorkspaceDataInfo.R
@@ -0,0 +1,4 @@
+flowWorkspaceDataInfo <-
+function(){
+cat("This package contains fcs data files and an xml workspace for testing the flowWorkspace package and building the flowWorkspace vignette.\n These can be located in the inst/extdata directory of the flowWorkspaceData package.\n The sample files and workspace contain data from whole blood. The primary purpose of this data set is to test the import code and export code in the flowWorkspace package.")}
+
