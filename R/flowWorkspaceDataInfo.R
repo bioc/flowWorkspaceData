f70a6d4bde7722d41186508c57216b2ef47f88d7
Mike Jiang <wjiang2@fhcrc.org>
2013-09-20 22:35:10 +0000

    modify package info due to the change to the data sets
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/flowWorkspaceData@2450 db2202da-8704-0410-b924-b125b3cd84ef

--- R/flowWorkspaceDataInfo.R
+++ R/flowWorkspaceDataInfo.R
@@ -1,4 +1,4 @@
 flowWorkspaceDataInfo <-
 function(){
-cat("This package contains fcs data files and an xml workspace for testing the flowWorkspace package and building the flowWorkspace vignette.\n These can be located in the inst/extdata directory of the flowWorkspaceData package.\n The sample files and workspace contain data from whole blood. The primary purpose of this data set is to test the import code and export code in the flowWorkspace package.")}
+cat("This package contains fcs data files and xml workspaces for testing the flowWorkspace and openCyto packages and building the their vignettes.\n These can be located in the inst/extdata directory of the flowWorkspaceData package.\n The sample files and workspaces contain data from whole blood. The primary purpose of this data set is to test the import code and export code in the flowWorkspace package and running automated gating pipeline in the openCyto package.")}
 
